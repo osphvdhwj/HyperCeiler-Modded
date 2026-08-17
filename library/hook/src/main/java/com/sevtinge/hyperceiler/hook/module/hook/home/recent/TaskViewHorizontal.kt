7210f158cf141272caad6c3785391d40b9ad6318
Harry <harry@example.com>
2026-08-18 00:11:34 +0530

    chore(debug): add exploratory hooks for MIUI classes

--- library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/hook/home/recent/TaskViewHorizontal.kt
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/hook/home/recent/TaskViewHorizontal.kt
@@ -28,18 +28,21 @@ import com.sevtinge.hyperceiler.hook.utils.hookAfterMethod
 object TaskViewHorizontal : BaseHook() {
     override fun init() {
 
-        val value1 = mPrefsMap.getInt("task_view_horizontal1", 100).toFloat() / 100
-        val value2 = mPrefsMap.getInt("task_view_horizontal2", 100).toFloat() / 100
-        if (value1 == 1f && value2 == 1f) return
-        "com.miui.home.recents.views.TaskStackViewsAlgorithmHorizontal".hookAfterMethod(
-            "scaleTaskView", RectF::class.java,
-        ) {
-            "com.miui.home.recents.util.Utilities".findClass().callStaticMethod(
-                "scaleRectAboutCenter",
-                it.args[0],
-                if (it.thisObject.callMethod("isLandscapeVisually") as Boolean) value2 else value1
-            )
-        }
+        try {
+            "com.miui.home.recents.views.TaskStackViewsAlgorithmHorizontal".findClass().declaredMethods.forEach { method ->
+                com.sevtinge.hyperceiler.hook.utils.log.XposedLogUtils.logI("HyperHand-Explore", "Found method in TaskStackViewsAlgorithmHorizontal: " + method.name)
+            }
+        } catch (e: Exception) {}
 
-    }
+        try {
+            "com.miui.home.launcher.RecentsAndFsGestureUtils".findClass().declaredMethods.forEach { method ->
+                com.sevtinge.hyperceiler.hook.utils.log.XposedLogUtils.logI("HyperHand-Explore", "Found method in RecentsAndFsGestureUtils: " + method.name)
+            }
+        } catch (e: Exception) {}
+        
+        try {
+            "com.miui.home.recents.RecentsModel".findClass().declaredMethods.forEach { method ->
+                com.sevtinge.hyperceiler.hook.utils.log.XposedLogUtils.logI("HyperHand-Explore", "Found method in RecentsModel: " + method.name)
+            }
+        } catch (e: Exception) {}
 }
